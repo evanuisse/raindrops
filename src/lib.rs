f13ae2105605a05a59b489983eda097938053428
evanuisse <77765376evanuisse@github.com>
2021-02-18 00:48:00 -0700

    raindrops

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+pub fn raindrops(n: u32) -> String {
+    let mut rainSound = String::new();
+
+    if n % 3 == 0 {
+      rainSound += "Pling";
+    }
+
+    if n % 5 == 0 {
+      rainSound += "Plang;
+    }
+
+    if n % 7 == 0 {
+      rainSound += "Plong";
+    }
+    
+    if rainSound.len() == 0 {
+	rainSound = n.toString();
+	}
+
+    sound
+
+}
+
